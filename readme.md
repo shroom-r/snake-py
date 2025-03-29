39018bdc238b17a0147c653c1d23bb65f6b95df2
Bobyte5923 <bob30aaa@gmail.com>
2025-03-29 08:44:25 +0100

    rien

--- readme.md
+++
@@ -1,5 +0,0 @@
-# Python snake project
-
-CAS-IDD 2025 / Python
-
-Rafael Teixeira & Ayman Akram
--- snack.py
+++ snack.py
@@ -1,3 +1,15 @@
+"""
+Contient la classe Snack qui génère la position aléatoire de la pomme en évitant le serpent.
+"""
+import random
+from point import Point #  classe utilitaire qui représente un point (Y, X) dans la grille du jeu.
+
 class Snack:
-    def __init__(self):
-        pass
+    def __init__(self, screen_height, screen_width, snake_body): # screen_height et screen_width : taille de l'écran (hauteur et largeur).
+        self.position = self.generate_position(screen_height, screen_width, snake_body)
+
+    def generate_position(self, height, width, snake_body):
+        while True:
+            pos = Point(random.randint(1, height - 2), random.randint(1, width - 2))
+            if pos not in snake_body: # pas sur le serpent
+                return pos
